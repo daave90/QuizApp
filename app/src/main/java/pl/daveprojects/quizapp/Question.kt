768e37c3b70f528853e8a1b9e7354eb59748498c
Dave <maciak.dawid.96@gmail.com>
2021-02-24 14:58:59 +0100

    Create Questions data

---
+++ app/src/main/java/pl/daveprojects/quizapp/Question.kt
@@ -0,0 +1,12 @@
+package pl.daveprojects.quizapp
+
+data class Question(
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionOThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+)
