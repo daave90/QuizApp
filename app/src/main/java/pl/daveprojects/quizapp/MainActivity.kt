23afcdd8c125ece55da4e5d59a0ddd298bd342a2
Dave <maciak.dawid.96@gmail.com>
2021-02-24 14:17:51 +0100

    Create UI for welcome screen and hide status bar in MainActivity.kt

--- app/src/main/java/pl/daveprojects/quizapp/MainActivity.kt
+++ app/src/main/java/pl/daveprojects/quizapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package pl.daveprojects.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
