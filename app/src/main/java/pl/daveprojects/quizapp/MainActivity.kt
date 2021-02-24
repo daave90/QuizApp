b51ab11c1a91db88f919d59d0bea6ec6ba4621de
Dave <maciak.dawid.96@gmail.com>
2021-02-24 14:32:38 +0100

    Create new activity and setup start button to display this QuizQuestionActivity.kt

--- app/src/main/java/pl/daveprojects/quizapp/MainActivity.kt
+++ app/src/main/java/pl/daveprojects/quizapp/MainActivity.kt
@@ -1,13 +1,29 @@
 package pl.daveprojects.quizapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        btnStart.setOnClickListener {
+            val etText = findViewById<AppCompatEditText>(R.id.et_text)
+            if (etText.text.isNullOrEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuizQuestionActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
---
+++ app/src/main/java/pl/daveprojects/quizapp/QuizQuestionActivity.kt
@@ -0,0 +1,11 @@
+package pl.daveprojects.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizQuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_question)
+    }
+}
